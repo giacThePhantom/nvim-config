083ce59bed6e4043c07ac698840c2f1d45160914
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-03-27 12:19:11 +0200

    snippets

--- init.lua
+++ init.lua
@@ -10,6 +10,9 @@ require('plugins/neodev')
 require('plugins/nvim-cmp')
 require('plugins/mason')
 require('options/vimtex')
+require('options/luasnip')
+require('mappings/luasnip')
+require('snippets/latex')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
