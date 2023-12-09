f8c0937bd3c706a22329b413a2ba03c8d13a3a62
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-12-09 23:27:37 +0100

    Configured vimtex

--- lua/packages/vimtex.lua
+++ lua/packages/vimtex.lua
@@ -18,3 +18,20 @@ vim.g.vimtex_toc_config = {
 
 vim.wo.conceallevel = 2
 vim.g.tex_conceal = 'abdmgs'
+
+vim.g.vimtex_compiler_latexmk = {
+         aux_dir = '',
+         out_dir = 'build',
+         callback = 1,
+         continuous = 1,
+         executable = 'latexmk',
+         hooks = {},
+         options = {
+           '-verbose',
+           '-file-line-error',
+           '-synctex=1',
+           '-interaction=nonstopmode',
+         },
+        }
+
+vim.g.vimtex_quickfix_open_on_warning = 0
